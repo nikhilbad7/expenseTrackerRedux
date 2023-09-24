c34d40c02a6a9511514fe470d8035e0aa842083f
nikhil <nikhil.badani@gmail.com>
2023-09-24 22:00:05 +0530

    ExpenseForm created

--- expense_tracker_redux/src/components/NewExpense/ExpenseForm.js
+++ expense_tracker_redux/src/components/NewExpense/ExpenseForm.js
@@ -1,6 +1,57 @@
+import { useState } from "react"
+
 export const ExpenseForm = (props) => {
+    const [enteredTitle,setEnteredTitle] = useState("");
+    const [enteredAmount,setEnteredAmount] = useState("");
+    const [enteredDate,setEnteredDate] = useState("");
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    }
+
+    const amountChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    }
+
+    const dateChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: +enteredAmount,
+            date: new Date(enteredDate)
+        }
+
+        props.onSaveExpenseDate(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    }
+
     return (
-    <div>
-        Form
-    </div>)
+        <form onSubmit={submitHandler}>
+            <div>
+                <div>
+                    <label>Title</label>
+                    <input type='text' value={enteredTitle} onChange={titleChangeHandler}></input>
+                </div>
+                <div>
+                    <label>Amount</label>
+                    <input type='number' min='0.01' step='0.01' value={enteredAmount} onChange={amountChangeHandler}></input>
+                </div>
+                <div>
+                    <label>Date</label>
+                    <input type='date' min='2019-01-01' max='2023-12-31' value={enteredDate} onChange={dateChangeHandler}></input>
+                </div>
+            </div>
+            <div>
+                <button type="button" onClick={props.onCancel}>Cancel</button>
+                <button type="submit">Add Expense</button>
+            </div>
+        </form>
+    )
 }
